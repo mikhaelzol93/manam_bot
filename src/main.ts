d3281ea888c325d92eaf560fba78a3732ca54ea2
Mikhail Zolotarev <mikhaelzol93@gmail.com>
2026-06-26 14:04:46 +0300

    chore: add basic main file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import { Bot } from "grammy";
 
+function main() {
+  const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+  if (!telegramBotToken) return;
+
+  const bot = new Bot(telegramBotToken);
+
+  bot.start({ timeout: 90 });
+}
+
+main();
